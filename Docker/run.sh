0bb61e37eae7363cdcbbd27a9b04e182e60b71ec
LeeDogyeom <lywoo@ivis.ai>
2025-06-18 15:45:59 +0900

    gui 추가

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,1 +1,8 @@
-docker run -it antlr-zsh-env
+# 호스트에서 DISPLAY 변수와 X 권한 공유
+xhost +local:root
+docker run -it \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --name antlr-gui \
+  antlr-zsh-env
+
